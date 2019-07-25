75c85febf245ac371549ce6806211bfe4b392a61
michael-read <michael.read@lightbend.com>
2019-07-25 13:52:30 -0700

    remove unused import

--- src/main/scala/com/lightbend/artifactstate/endpoint/JsonSupport.scala
+++ src/main/scala/com/lightbend/artifactstate/endpoint/JsonSupport.scala
@@ -1,7 +1,6 @@
 package com.lightbend.artifactstate.endpoint
 
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
-import com.lightbend.artifactstate.actors.ArtifactStateEntityActor.Okay
 import com.lightbend.artifactstate.endpoint.ArtifactStatePocAPI._
 import spray.json.DefaultJsonProtocol
 
